9216102c22b39335a7b82ee083eda8fbbe56c361
Guandjoy <guandjoy@gmail.com>
2019-12-10 04:17:19 +0200

    Setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve("build"),
+    filename: "index.js",
+    libraryTarget: "commonjs2"
+  },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+  }
+};
