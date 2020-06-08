f1df796925f439471af4bbfea948a82b483ca5d5
Yurko Turskiy <guandjoy@gmail.com>
2020-06-08 23:50:12 +0300

    Set up storybook

---
+++ website/stories/2-Header.stories.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from "../src/components/DesignSystem/Header"
+
+export default {
+  title: "Header",
+  component: Header,
+}
+
+export const H1 = () => <Header>Hello Button</Header>
