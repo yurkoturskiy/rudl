e29fd05a8aedc3378826de16ab7ebb4409fec0a2
Yurko Turskiy <guandjoy@gmail.com>
2020-06-09 10:10:59 +0300

    Update seo.tsx

--- website/src/components/seo.tsx
+++ website/src/components/seo.tsx
@@ -6,11 +6,22 @@
  */
 
 import React from "react"
-import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, meta, title }) {
+interface SEO {
+  description: string
+  lang: string
+  meta: Meta[]
+  title: string
+}
+
+interface Meta {
+  name: string
+  content: string
+}
+
+function SEO({ description, lang, meta, title }: SEO) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -76,13 +87,6 @@ SEO.defaultProps = {
   lang: `en`,
   meta: [],
   description: ``,
-}
-
-SEO.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
 }
 
 export default SEO
