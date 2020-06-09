8a6f6b35a4123ccb5e7239c47833e88e61a4a21a
Yurko Turskiy <guandjoy@gmail.com>
2020-06-09 06:08:14 +0300

    Set up gatsby-plugin-theme-ui

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-theme-ui`,
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
