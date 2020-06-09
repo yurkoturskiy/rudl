5aa527d8a4c8cfe2621625717b820d5757a7fbab
Yurko Turskiy <guandjoy@gmail.com>
2020-06-09 10:44:08 +0300

    Update body style

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-theme-ui`,
+    // `gatsby-plugin-theme-ui`,
     `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
