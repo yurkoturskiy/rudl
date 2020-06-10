9590e9b14e926c67450c4eef3f75fe93b7cab442
Yurko Turskiy <guandjoy@gmail.com>
2020-06-10 08:16:36 +0300

    Create types for useResponsiveRef hook

--- src/useResponsiveRef/index.tsx
+++ src/useResponsiveRef/index.tsx
@@ -5,18 +5,22 @@
 
 import React, { useEffect, useCallback, useRef, useState } from "react";
 
-function useResponsiveRef(action) {
-  const [width, setWidth] = useState(null);
-  const ref = useRef(null);
+function useResponsiveRef(
+  action: (newWidth: number) => void
+): [React.RefObject<HTMLDivElement>, number | undefined] {
+  const [width, setWidth] = useState<number>();
+  const ref = useRef<HTMLDivElement>(null);
   const onWidthResize = useCallback(action, [action]);
   const handleResize = useCallback(() => {
-    const newWidth = ref.current.offsetWidth;
+    const element = ref.current as HTMLDivElement;
+    const newWidth = element.offsetWidth;
     setWidth(newWidth);
     width !== newWidth && onWidthResize(newWidth);
   }, [onWidthResize, width]);
 
   useEffect(() => {
-    setWidth(ref.current.offsetWidth);
+    const element = ref.current as HTMLDivElement;
+    setWidth(element.offsetWidth);
     window.addEventListener("resize", handleResize);
     return () => window.removeEventListener("resize", handleResize);
   }, [handleResize]);
