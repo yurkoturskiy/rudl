21bf6837f1b2950294e7c4b177f4aaf3e3f7e7d9
Yurko Turskiy <guandjoy@gmail.com>
2020-06-10 03:29:53 +0300

    Add position to Box

--- website/src/components/DesignSystem/Box.tsx
+++ website/src/components/DesignSystem/Box.tsx
@@ -6,14 +6,17 @@ import {
   SpaceProps,
   layout,
   LayoutProps,
+  position,
+  PositionProps,
 } from "styled-system"
 
-type Props = ColorProps & SpaceProps & LayoutProps
+type Props = ColorProps & SpaceProps & LayoutProps & PositionProps
 
 const Box = styled.div<Props>`
   ${color}
   ${space}
   ${layout}
+  ${position}
 `
 
 export default Box
